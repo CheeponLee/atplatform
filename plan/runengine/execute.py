324478ad78b62bd8dc26e576799d45c9365f55df
CheeponLee <CheeponLee@gmail.com>
2013-01-28 20:19:13 +0800

    upload

--- plan/runengine/execute.py
+++ plan/runengine/execute.py
@@ -5,12 +5,15 @@ from atplatform.plan.runengine.exceptioncatch import *
 from atplatform.plan.commonparam import *
 import time
 import atplatform.plan.sharedobject as so
+import os
+import random
+import shutil
 #import runningmanager as rm
 
 
 #run('case1',{'Browsername':'firefox','Browserversion':'10.0','Platform':'WINDOWS','Findtimeout':30,'Webdriverhub':'http://localhost:8888/wd/hub','Javascriptenabled':'True'})
-def run(casename,config,name,queue):
-	th=Process(target=exec_case, args=(casename,config,name,queue))
+def run(casename,config,name,queue,planname=None):
+	th=Process(target=exec_case, args=(casename,config,name,queue,planname))
 	th.daemon=True
 	th.start()
 	so.runmanagerlog.info('start a process for excute:'+str(name))
@@ -18,7 +21,11 @@ def run(casename,config,name,queue):
 	#exec_case(casename,config)
 
 @exceptioncatch
-def exec_case(casename,config,name,queue):
+def exec_case(casename,config,name,queue,planname=None):
+	os.setuid(restricteduserid)
+	distdir=exctmpdir+str(planname)+'/'+str(casename)+'_'+str(int(time.time()))+str(random.randint(1,1000))
+	shutil.copytree(home+'cases/'+casename,distdir,ignore=shutil.ignore_patterns(str(casename)+'.py*','objectmapping.py*'))
+	os.chdir(exctmpdir+str(planname)+'/'+str(casename))
 	case=get_case(casename,config,queue)
 	case.init()
 	try:
@@ -39,6 +46,8 @@ def exec_case(casename,config,name,queue):
 		raise Exception(str(e),pic)
 	finally:
 		case.clean()
+		if os.path.exists(distdir):
+			shutil.rmtree(distdir)
 
 def get_case(casename,config,queue):
 	exec('from atplatform.plan.cases.'+casename+ ' import '+casename)
